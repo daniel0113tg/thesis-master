3acf6d75b362e87bdfb81e41abf7838d7ca1701e
Daniel Taco <daniel.taco@epn.edu.ec>
2021-02-18 18:25:56 -0500

    cambios5

--- thesis-master/test_one_file.py
+++ thesis-master/test_one_file.py
@@ -105,7 +105,7 @@ def test_one_file(path_to_videos, video_id, groundtruth_file, timesteps, image_d
         X = np.array(X)
 
         # package the input data as a batch of size 1
-        X = np.expand_dims(X, axis=1)       # a batch of 1, adding an extra dimension
+        X = np.expand_dims(X, axis=-1)       # a batch of 1, adding an extra dimension
 
         # process...
         answer = model.predict(X)
