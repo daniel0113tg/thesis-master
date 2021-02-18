e466752377c920e7423b4f1c46c17099b2da72d3
Daniel Taco <daniel.taco@epn.edu.ec>
2021-02-18 18:48:48 -0500

    cambios11

--- thesis-master/test_one_file.py
+++ thesis-master/test_one_file.py
@@ -97,6 +97,12 @@ def test_one_file(path_to_videos, video_id, groundtruth_file, timesteps, image_d
 
         # get the CNN features Cuesiton
         X = []
+        print(enumerate(windows_ndx))
+        a, b = windows_ndx.shape
+        print("a")
+        print( a)
+        print("b")
+        print (b)
         for (j, ndx) in enumerate(window_ndx):
             dt = np.load(cnn_files[ndx])
             dt = np.array(dt['X']) 
@@ -107,7 +113,6 @@ def test_one_file(path_to_videos, video_id, groundtruth_file, timesteps, image_d
             print (b)
             print(len(dt))    # has shape (timesteps, CNN feature vector length)
             X.append(dt[0, ...])
-        print(X)
         X = np.array(X)
         a, b = X.shape
         print("a")
