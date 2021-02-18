0b89c2bb3b86fb5480c2f53f36de7955261786c1
Daniel Taco <daniel.taco@epn.edu.ec>
2021-02-18 18:34:53 -0500

    cambios8

--- thesis-master/test_one_file.py
+++ thesis-master/test_one_file.py
@@ -103,11 +103,10 @@ def test_one_file(path_to_videos, video_id, groundtruth_file, timesteps, image_d
             print(len(dt))    # has shape (timesteps, CNN feature vector length)
             X.append(dt[0, ...])
         X = np.array(X)
-
-        # package the input data as a batch of size 1
-        X = np.expand_dims(X, axis=1)       # a batch of 1, adding an extra dimension
         a, b = X.shape
         X = X.reshape(a, b, 1)
+        # package the input data as a batch of size 1
+        X = np.expand_dims(X, axis=1)       # a batch of 1, adding an extra dimension
         # process...
         answer = model.predict(X)
 
