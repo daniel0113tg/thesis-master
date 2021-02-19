f3b842c9b6a8a4b294ea67a5bc79e43cc2f42db6
Daniel Taco <daniel.taco@epn.edu.ec>
2021-02-18 19:38:14 -0500

    cambios19

--- thesis-master/test_one_file.py
+++ thesis-master/test_one_file.py
@@ -100,9 +100,11 @@ def test_one_file(path_to_videos, video_id, groundtruth_file, timesteps, image_d
      
         print(type(window_ndx))
         for (j, ndx) in enumerate(window_ndx):
-            
+
             print(cnn_files[ndx])
             dt = np.load(cnn_files[ndx])
+            a,b = dt.shape()
+            print(a,b)
             dt = np.array(dt['X']) 
    # has shape (timesteps, CNN feature vector length)
             X.append(dt[0, ...])
